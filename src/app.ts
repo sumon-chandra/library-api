5eeb1a4db6fa4da121fcb42bf42a02211a94f3bb
Sumon Chandra <80484412+sumon-chandra@users.noreply.github.com>
2025-07-05 22:06:49 +0600

    Remove unnecessary CORS methods and headers configuration in app.ts

--- src/app.ts
+++ src/app.ts
@@ -4,14 +4,12 @@ import bookRoutes from './modules/books/book.route';
 import borrowRoutes from './modules/borrow/borrow.route';
 
 const app = express();
-app.use(express.json());
 app.use(cors(
     {
         origin: 'http://localhost:5173',
-        methods: ['GET', 'POST', 'PUT', 'DELETE'],
-        allowedHeaders: ['Content-Type', 'Authorization']
     }
 ))
+app.use(express.json());
 
 app.get('/', (req, res) => {
     res.send("Welcome to the Library Management System API. Go to /api/books for book operations and /api/borrow for borrowing operations.");
