f5db5b97755246d11ac9a2127603fa67962ad9c0
Sumon Chandra <80484412+sumon-chandra@users.noreply.github.com>
2025-07-11 00:47:46 +0600

    Refactor CORS configuration to explicitly define allowed origins, methods, and headers

--- src/app.ts
+++ src/app.ts
@@ -4,7 +4,11 @@ import bookRoutes from './modules/books/book.route';
 import borrowRoutes from './modules/borrow/borrow.route';
 
 const app = express();
-app.use(cors())
+app.use(cors({
+    origin: "*", // allow all origins
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+}));
 
 app.use(express.json());
 
